ead9a6bb711ae908dc3a0c7af74872d1c2f68e45
Emanuele <emanu360@hotmail.it>
2024-07-13 15:38:06 +0200

    inserzione di << per introdurre le variabili

--- main.cpp
+++ main.cpp
@@ -11,11 +11,14 @@ int main() // la funzione main serve come primo passo per il
     anno = 2024; // assegnazione
 
     // inizializzazione di variabile
-    int eta = 31;                                 // ha bisogno di int una variabile, senno' da errore
-    std::cout << "ciao ho 100 anni" << std::endl; // endl mette a capo il prossimo comando
-    std::cout << "ciao ho 100 anni da settembre" << std::endl;
+    int eta = 32;                                           // ha bisogno di int una variabile, senno' da errore
+    std::cout << "ciao ho " << eta << " anni" << std::endl; // endl mette a capo il prossimo comando
+    std::cout << "ciao ho " << eta << " anni da settembre" << std::endl;
     // std::cout << "seconda stampa prova";
+    std::cout << "anche tu hai " << eta << " anni?" << std::endl;
     // std standard si trova dentro iostream, cout significa console out,
     // mostra a console,conviene leggerlo da destra verso sinistra
+    eta = 33;
+    std::cout << "anche tu hai " << eta << " anni?" << std::endl;
     return 0; // return zero porta fuori dalla fgunzione, come se fosse un OK puoi uscire! m a deve essere intero in questo caso
 }
